da02fee62a3b506a36f87aefb84fe707a15c6ec7
----- ---- <purposedriven@s-MacBook-Pro.local>
2016-06-26 17:32:23 -0700

    added createboard function

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,4 +15,12 @@ if (cardTwo === cardFour) {
 	alert("Sorry, try again.")
 }
 
-/* Added alerts */
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		  cardElement.className = 'card';
+		    board.appendChild(cardElement);
+  }
+}
